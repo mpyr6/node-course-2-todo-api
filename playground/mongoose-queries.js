7de0fc89058bf7e19ae0a29a4857210eaf337a05
Chris Wiesen <mpyr6@yahoo.com>
2017-08-08 18:59:26 -0700

    Add queries Playground

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -2,6 +2,7 @@ const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
 // var id = '5989477e8d4cff6c0543870211';
 //
@@ -27,3 +28,12 @@ const {Todo} = require('./../server/models/todo');
 //   }
 //   console.log('Todo by id', todo);
 // }).catch((e) => console.log(e));
+
+var id = '59894e9a82ded9b2a05cbd9e';
+
+User.findById(id).then((user) => {
+  if(!user) {
+    return console.log('User not found');
+  }
+  console.log('User by id', user);
+}).catch((e) => console.log(e));
