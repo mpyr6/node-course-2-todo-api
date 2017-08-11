d06d690935b7a6e531cc7fee6afc5cdaea8a12b0
Chris Wiesen <mpyr6@yahoo.com>
2017-08-10 18:50:02 -0700

    Added test for validating user id

--- server/server.js
+++ server/server.js
@@ -29,6 +29,22 @@ app.get('/todos',(req, res) => {
   });
 });
 
+// GET /todos/12341234
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  //Validate ID using isValid
+    // Respond with 404 - send back empty body
+
+  // findById
+    // Success
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    // Error
+      //400 - and send empty body back
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
