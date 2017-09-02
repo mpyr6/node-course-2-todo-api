a8f58a09d7fae9b2b428c66dc159908befb7ec6f
Chris Wiesen <mpyr6@yahoo.com>
2017-09-02 15:05:36 -0700

    Added delete functionality to server.js

--- server/server.js
+++ server/server.js
@@ -58,6 +58,30 @@ app.get('/todos/:id', (req,res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // validate the ID - not valid? return 404
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  // remove todo by id
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    };
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+    // Success
+      // if no doc, send 404
+      //if doc, send doc back with 200
+    // Error
+      // 400 with empty body
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
